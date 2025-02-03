b599fd7adf9986323b823def788fad0ac7a61129
Manavv Vakharia <vakharia.2603@gmail.com>
2025-02-02 23:30:48 -0800

    Inverts the tech stack buttons

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -6,37 +6,37 @@ import { skillsItems} from "../data/index.ts";
 <template>
 
 <section class="min-h-fit bg-gray-800" id="skills">
-      <div class="px-5 py-14 md:container">
-        <h2 class="subtitle text-white" data-aos="fade-down">
-          Skills
-        </h2>
-        <h4 class="title text-black" data-aos="fade-down">
-          Tech-Stack
-        </h4>
-        <br />
-        <div class="flex flex-wrap justify-center gap-4">
-            <div v-for="(item, skillIndex) in skillsItems" :key="skillIndex" 
-              class="group relative flex w-full max-w-sm items-center gap-5 rounded-md border-2 border-white bg-gray-950 p-5 duration-200 hover:border-gray-600 sm:cursor-pointer"
-              data-aos="fade-up"
-              :data-aos-delay="index * 400"
-            >
-              <div>
-                <img
-                  :src="item.logo"
-                  :alt="item.name"
-                  class="w-32 duration-200 group-hover:scale-200"
-                />
-              </div>
-              <div>
-                <h6 class="text-gray-100">{{ item.name }}</h6>
-                <p class="italic text-gray-300">{{ item.description }}</p>
-                <div class="absolute right-3 top-3 text-xl text-yellow-500">
-                  <!-- <ArrowUpRightIcon class="size-6 text-yellow-600" /> -->
-                </div>
-              </div>
+    <div class="px-5 py-14 md:container">
+      <h2 class="subtitle text-white" data-aos="fade-down">
+        Skills
+      </h2>
+      <h4 class="title text-black" data-aos="fade-down">
+        Tech-Stack
+      </h4>
+      <br />
+      <div class="flex flex-wrap justify-center gap-4">
+        <div v-for="(item, skillIndex) in skillsItems" :key="skillIndex"
+          class="group relative flex w-full max-w-sm items-center gap-5 rounded-md border-2 border-white bg-gray-950 p-5 duration-200 hover:bg-white hover:border-black hover:text-black sm:cursor-pointer"
+          data-aos="fade-up"
+          :data-aos-delay="skillIndex * 400"
+        >
+          <div>
+            <img
+              :src="item.logo"
+              :alt="item.name"
+              class="w-32 duration-200 group-hover:scale-200 group-hover:filter group-hover:invert"
+            />
+          </div>
+          <div>
+            <h6 class="text-gray-100 group-hover:text-black">{{ item.name }}</h6>
+            <p class="italic text-gray-300 group-hover:text-black">{{ item.description }}</p>
+            <div class="absolute right-3 top-3 text-xl text-yellow-500">
+              <!-- <ArrowUpRightIcon class="size-6 text-yellow-600" /> -->
             </div>
+          </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
 </template>
